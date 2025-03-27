31fc1e264f0f5bee971c0310b431ec5a47bcc29b
jiachenHan <jiachen_han@tju.edu.cn>
2025-03-27 21:35:35 +0800

    vul exp

---
+++ script/exp/vul/gen_query_vul4j.py
@@ -0,0 +1,138 @@
+import asyncio
+import json
+import random
+from pathlib import Path
+from typing import Generator
+
+import utils
+from app.communication import PatternInput
+from app.pipeline.abstract import llm_abstract
+from interface.java.run_java_api import java_extract_pattern, java_abstract, java_generate_query
+from interface.llm.llm_pool import AsyncLLMPool
+from utils.config import set_config, LoggerConfig
+
+_logger = LoggerConfig.get_logger(__name__)
+
+
+def extract_pattern(_jar: str, _group_path: Path, _pattern_path: Path, _pattern_info_path: Path):
+    pattern_ori_path = _pattern_path / "ori" / f"{_group_path.stem}.ser"
+    pattern_info_input_path = _pattern_info_path / "input" / f"{_group_path.stem}.json"
+    java_extract_pattern(30, _group_path, pattern_ori_path, pattern_info_input_path, _jar)
+
+
+async def async_abstract_pattern(
+        _llm_pool: AsyncLLMPool,  # 使用异步池替代单个LLM
+        _jar: str,
+        _group_path: Path,
+        _pattern_path: Path,
+        _pattern_info_path: Path
+):
+    error_info = json.load(open(_group_path / "info.json", 'r'))["issue"].strip()
+
+    pattern_ori_path = _pattern_path / "ori" / f"{_group_path.stem}.ser"
+    pattern_abs_path = _pattern_path / "abs" / f"{_group_path.stem}.ser"
+    pattern_info_input_path = _pattern_info_path / "input" / f"{_group_path.stem}.json"
+    pattern_info_output_path = _pattern_info_path / "output" / f"{_group_path.stem}.json"
+
+    if pattern_abs_path.exists():
+        return
+
+    pattern_input = PatternInput.from_file(pattern_info_input_path)
+    pattern_input.set_error_info(error_info)
+    await _llm_pool.async_run(
+        llm_abstract,
+        pattern_input,
+        pattern_info_output_path
+    )
+
+    java_abstract(30, pattern_ori_path, pattern_info_output_path, pattern_abs_path, _jar)
+
+
+def generate_query(_jar: str, _group_path: Path, _pattern_path: Path, _dsl_path: Path):
+    pattern_abs_path = _pattern_path / "abs" / f"{_group_path.stem}.ser"
+    dsl_output_path = _dsl_path / f"{_group_path.stem}.kirin"
+
+    java_generate_query(30, pattern_abs_path, dsl_output_path, _jar)
+
+
+def get_ab_case(_pattern_path: Path, _data_path: Path) -> Generator[Path, None, None]:
+    _abs_pattern_path = _pattern_path / "ori"
+    for abs_pat in _abs_pattern_path.rglob("*.ser"):
+        case_name = abs_pat.stem
+        group_name = abs_pat.parent.stem
+        checker_name = abs_pat.parent.parent.stem
+        yield _data_path / checker_name / group_name / case_name
+
+
+def get_code_pairs(_path: Path) -> Generator[Path, None, None]:
+    for group in _path.iterdir():
+        if not group.is_dir():
+            continue
+        yield group
+
+
+async def process_single_case(
+        llm_pool: AsyncLLMPool,
+        jar: str,
+        group: Path,
+        pattern_path: Path,
+        dsl_path: Path,
+        pattern_info_path: Path
+):
+    extract_pattern(jar, group, pattern_path, pattern_info_path)
+    # 异步执行核心步骤
+    await async_abstract_pattern(llm_pool, jar, group, pattern_path, pattern_info_path)
+    # 同步后续步骤
+    generate_query(jar, group, pattern_path, dsl_path)
+
+
+async def main():
+    _config = set_config("ppinfra")
+    jar_path = _config.get("jar_path")
+
+    llm_pool = AsyncLLMPool([
+        (_config.get("openai").get("base_url"),
+         api_key,
+         _config.get("openai").get("model"))
+        for api_key in _config.get("openai").get("api_keys")
+    ])
+
+    # 创建并行任务（限制最大并发数）
+    sem = asyncio.Semaphore(5)  # 根据API总限制调整
+
+    dataset_name = "vul4j"
+    dataset_path = Path("/data/jiangjiajun/CodeNavi-DSL/data") / dataset_name
+
+    pattern_path = utils.config.get_pattern_base_path() / dataset_name
+    pattern_info_path = utils.config.get_pattern_info_base_path() / dataset_name
+    dsl_path = utils.config.get_dsl_base_path() / dataset_name
+
+    groups = get_code_pairs(dataset_path)
+
+    tasks = []
+    for group in groups:
+        group_name = group.stem
+
+        dsl_group_path = dsl_path / group_name
+        if dsl_group_path.exists():
+            _logger.info(f"{str(dsl_group_path)} already exists")
+            continue
+
+        task = asyncio.create_task(
+            process_single_case(
+                llm_pool,
+                jar_path,
+                group,
+                pattern_path,
+                dsl_path,
+                pattern_info_path
+            )
+        )
+        task.add_done_callback(lambda _: sem.release())
+        await sem.acquire()
+        tasks.append(task)
+
+    await asyncio.gather(*tasks)
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ script/exp/vul/scan_repos_vul4j.py
@@ -0,0 +1,78 @@
+import json
+import platform
+import random
+import stat
+from pathlib import Path
+from typing import Generator
+
+from interface.java.run_java_api import kirin_engine
+from utils.config import LoggerConfig
+
+_logger = LoggerConfig.get_logger(__name__)
+
+
+def get_random_repo_path(_dsl_path: Path, _repos_base_path: Path) -> Path:
+    _case_name = _dsl_path.stem
+    _group_name = _dsl_path.parent.stem
+    _checker_name = _dsl_path.parent.parent.stem
+    _group_repo_path = _repos_base_path / _checker_name / _group_name
+
+    _random_case_path = random.choice([_case for _case in _group_repo_path.iterdir() if _case.is_dir() and _case_name != _case.stem])
+    return next(_random_case_path.iterdir(), None)
+
+
+def get_dsl_paths(_query_path: Path) -> Generator[Path, None, None]:
+    return _query_path.rglob("*.kirin")
+
+
+def split_java_package(dir_path: Path) -> Generator[Path, None, None]:
+    if platform.system() == "Windows":
+        try:
+            attrs = dir_path.stat().st_file_attributes
+            if (attrs & (stat.FILE_ATTRIBUTE_HIDDEN | stat.FILE_ATTRIBUTE_SYSTEM)) != 0:
+                return
+        except AttributeError:
+            return
+
+    if dir_path.name.startswith("."):
+        return
+
+    if dir_path.is_file():
+        if dir_path.name.endswith(".java"):
+            yield dir_path
+        return
+
+    all_java = all(child.is_file() and child.name.endswith(".java") for child in dir_path.iterdir())
+    if all_java:
+        yield dir_path
+    else:
+        for child in dir_path.iterdir():
+            yield from split_java_package(child)
+
+
+def detect_repo(_query_base_path: Path,
+                _repos_path: Path,
+                _results_path: Path):
+    engine_path = Path("D:/envs/kirin-cli-1.0.8_sp06-jackofext-obfuscate.jar")
+
+    for _dsl_path in get_dsl_paths(_query_base_path):
+        for _scanned_repo_path in _repos_path.iterdir():
+
+            _result_path = _results_path / _dsl_path.stem / _scanned_repo_path.stem
+
+            for index, pkg in enumerate(split_java_package(_scanned_repo_path)):
+                index_result_path = _result_path / f"{index}_error_kirin"
+                timeout = kirin_engine(300, engine_path, _dsl_path, pkg, index_result_path)
+                if timeout:
+                    print(f"Timeout in : {_dsl_path}")
+                    break
+
+
+if __name__ == '__main__':
+    dataset_name = "vul4j"
+    query_base_path = Path(f"E:/dataset/Navi/vul/ql/{dataset_name}")
+
+    repos_path = Path(f"E:/dataset/Navi/local_repos")
+    results_path = Path(f"E:/dataset/Navi/vul/results/{dataset_name}")
+
+    detect_repo(query_base_path, repos_path, results_path)
